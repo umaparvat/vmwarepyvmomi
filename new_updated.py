ec8065ebd6f802d23180ecd0359de15490152d4d
umaparvat <ka.umaparvathi.cse@gmail.com>
2019-10-22 13:49:20 +0530

    Update new_updated.py

--- new_updated.py
+++ new_updated.py
@@ -28,6 +28,8 @@ allpg=[item for item in  content.viewManager.CreateContainerView(content.rootFol
 
 dvs = get_obj(content, [vim.DistributedVirtualSwitch], 'RegionA01-vDS-COMP')
 
+
+#switch and portgroup selection
 selection_set=[]
 dvs_set=vim.dvs.DistributedVirtualSwitchSelection()
 dvs_set.dvsUuid = dvs.uuid
@@ -41,26 +43,31 @@ for pg in dvs.portgroup:
 bkp = content.dvSwitchManager.DVSManagerExportEntity_Task(selection_set)
 result = bkp.info.result
 
-dvs1 = get_obj(content, [vim.DistributedVirtualSwitch], 'RegionA01-vDS-COMP1')  
+#to restore the switch and portgroup changes
+entity_restore_config = content.dvSwitchManager.DVSManagerImportEntity_Task(result, 'applyToEntitySpecified')   
+#to check status
+entity_restore_config.info.state
+entity_restore_config.info.error
+#to check output
+entity_restore_config.info.result
 
-entity_restore_config = content.dvSwitchManager.DVSManagerImportEntity_Task(export_result,'createEntityWithOriginalIdentifier')        
-
-entity_restore_config = content.dvSwitchManager.DVSManagerImportEntity_Task(result)   
-
-
-
-#### switch backup 
+#### switch and portgroup new creation without hostmember #####
 
 dvs_set=vim.dvs.DistributedVirtualSwitchSelection()
 dvs_set.dvsUuid = dvs.uuid
 selection_set=[]
 selection_set.append(dvs_set)
+dvs_ss = vim.dvs.DistributedVirtualPortgroupSelection()
+dvs_ss.dvsUuid = dvs.uuid
+for pg in dvs.portgroup:
+    dvs_ss.portgroupKey.append(pg.key)
+    selection_set.append(dvs_ss)
 bkps = content.dvSwitchManager.DVSManagerExportEntity_Task(selection_set)
 res_switch=bkps.info.result
-# change the switch name
-res_switch.name = res_switch.name+"-new" 
+# change the switch name, since the first is switch in the res_switch list
+res_switch[0].name = res_switch[0].name+"-new" 
 #create a new switch
- entity_restore_config = content.dvSwitchManager.DVSManagerImportEntity_Task(res_switch, 'createEntityWithNewIdentifier')
+ entity_restore_config = content.dvSwitchManager.DVSManagerImportEntity_Task([res_switch[0]], 'createEntityWithNewIdentifier')
  entity_restore_config.info.result
  # output , the new switch id
  (vim.dvs.DistributedVirtualSwitchManager.ImportResult) {
@@ -73,15 +80,23 @@ res_switch.name = res_switch.name+"-new"
    importFault = (vim.fault.ImportOperationBulkFault.FaultOnImport) []
 }
  
+# store the Importentity result into a variable    
 switch_new=entity_restore_config.info.result
 switch_new.distributedVirtualSwitch[0]
 #output
 'vim.dvs.VmwareDistributedVirtualSwitch:dvs-338'
  
- # change the portgroup backup with new switch info
-for index,each_config in enumerate(result):
-    result[index].name=result[index].name+"-new"
-    result[index].container = switch_new.distributedVirtualSwitch[0]
+ # change the portgroup backup with new switch info, since res_switch[0] is switch, taking from portgroup
+result = []
+for each_port_config in res_switch[1:]:
+    each_port_config.name=each_port_config.name+"-new"
+    each_port_config.container = switch_new.distributedVirtualSwitch[0]
+    result.append(each_port_config)
     
-entity_restore_config = content.dvSwitchManager.DVSManagerImportEntity_Task(result, 'createEntityWithNewIdentifier')   
-    
+ #to create new portgroups on the new switch 
+entity_restore_port_config = content.dvSwitchManager.DVSManagerImportEntity_Task(result, 'createEntityWithNewIdentifier')   
+ #to check status
+entity_restore_port_config.info.state
+entity_restore_port_config.info.error
+#to check output
+entity_restore_port_config.info.result  
